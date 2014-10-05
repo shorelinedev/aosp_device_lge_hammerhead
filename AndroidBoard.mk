86d576c878ba1f02ce6f7913e84343d88358d639
Matthew <m.hanhimaki@gmail.com>
2014-10-05 00:54:41 -0400

    added msm directory flag to organize the kernel

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -17,11 +17,11 @@
 KERNEL_DEFCONFIG := hammerhead_defconfig
 KERNEL_DIR := kernel
 
--include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
+-include $(TOP)/$(KERNEL_DIR)/$(MSM_DIR)/AndroidKernel.mk
 
 # device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
 # So cp will do.
-.PHONY: $(PRODUCT_OUT)/kernel
+.PHONY: $(PRODUCT_OUT)/kernel/msm
 $(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
-	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel/msm
 
