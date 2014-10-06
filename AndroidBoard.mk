6f9a2b88434f4e6f85d4a0765d3d690c9846aa52
Matthew <m.hanhimaki@gmail.com>
2014-10-05 21:53:09 -0400

    revert msm hierarchy

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -15,13 +15,13 @@
 #
 
 KERNEL_DEFCONFIG := hammerhead_defconfig
-KERNEL_DIR := kernel/msm
+KERNEL_DIR := kernel
 
--include $(TOP)/$(KERNEL_DIR)/$(MSM_DIR)/AndroidKernel.mk
+-include $(TOP)/$(KERNEL_DIR)/AndroidKernel.mk
 
 # device.mk doesn't know about us, and we can't PRODUCT_COPY_FILES here.
 # So cp will do.
-.PHONY: $(PRODUCT_OUT)/kernel/msm
+.PHONY: $(PRODUCT_OUT)/kernel
 $(PRODUCT_OUT)/kernel: $(TARGET_PREBUILT_KERNEL)
-	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel/msm
+	cp $(TARGET_PREBUILT_KERNEL) $(PRODUCT_OUT)/kernel
 
