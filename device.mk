cd388279e73f779ac83faef5ec8964fd83f49533
Matthew <m.hanhimaki@gmail.com>
2014-10-24 09:02:09 -0400

    add twrp support

--- device.mk
+++ device.mk
@@ -20,6 +20,9 @@
 # Everything in this directory will become public
 
 PRODUCT_COPY_FILES += \
+    device/lge/hammerhead/twrp.fstab:recovery/root/etc/twrp.fstab
+
+PRODUCT_COPY_FILES += \
     device/lge/hammerhead/init.hammerhead.rc:root/init.hammerhead.rc \
     device/lge/hammerhead/init.hammerhead.usb.rc:root/init.hammerhead.usb.rc \
     device/lge/hammerhead/fstab.hammerhead:root/fstab.hammerhead \
